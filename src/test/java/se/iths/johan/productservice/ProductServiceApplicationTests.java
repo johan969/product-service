fcc7b332d795627a1183208a7d6d094c9eafe310
johan <johan.persson@iths.se>
2026-05-20 14:28:53 +0200

    Har lagt till properties och miljövaraiablar för renderdatabas. fixat så att aplikationen tar emot och skickar dto's

--- src/test/java/se/iths/johan/productservice/ProductServiceApplicationTests.java
+++ src/test/java/se/iths/johan/productservice/ProductServiceApplicationTests.java
@@ -2,8 +2,11 @@ package se.iths.johan.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import se.iths.johan.productservice.config.TestSecurityConfig;
 
 @SpringBootTest
+@Import(TestSecurityConfig.class)
 class ProductServiceApplicationTests {
 
     @Test
