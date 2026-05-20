c3bc49fefff2fa7df9a35ca0be3da68af4549e81
johan <johan.persson@iths.se>
2026-05-20 14:51:44 +0200

    Har lagt till properties och miljövaraiablar för renderdatabas. fixat så att aplikationen tar emot och skickar dto's

--- src/test/java/se/iths/johan/productservice/testConfig/TestSecurityConfig.java
+++ src/test/java/se/iths/johan/productservice/testConfig/TestSecurityConfig.java
@@ -3,14 +3,12 @@ package se.iths.johan.productservice.testConfig;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 import static org.mockito.Mockito.mock;
 
 @TestConfiguration
-@Import(TestSecurityConfig.class)
 public class TestSecurityConfig {
 
     @Bean
