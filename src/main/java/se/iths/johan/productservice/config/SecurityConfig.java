e77918e769d0fa50c88213241614595dd6d423e4
johan <johan.persson@iths.se>
2026-05-26 12:35:38 +0200

    commit

--- src/main/java/se/iths/johan/productservice/config/SecurityConfig.java
+++ src/main/java/se/iths/johan/productservice/config/SecurityConfig.java
@@ -63,11 +63,11 @@ public class SecurityConfig {
         return converter;
     }
 
-     //JwtDecoder används för att läsa våran jwt token, den gör detta genom att hämta den public nyckeln via "auth/jwks". (Finns i AuthController i vårt huvud projekt)
+    //JwtDecoder används för att läsa våran jwt token, den gör detta genom att hämta den public nyckeln via "auth/jwks". (Finns i AuthController i vårt huvud projekt)
     @Bean
     public JwtDecoder jwtDecoder(@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String authServerUrl) {
         NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder
-                .withJwkSetUri(authServerUrl)
+                .withJwkSetUri(authServerUrl + "/auth/jwks")
                 .build();
         jwtDecoder.setJwtValidator(
                 JwtValidators.createDefaultWithIssuer(authServerUrl));
