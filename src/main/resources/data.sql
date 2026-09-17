5e6809135790b106fac280bbeea70e2383d1db5e
johan969 <johan.persson@iths.se>
2026-09-17 13:44:32 +0200

    uppdaterat properties, för local testning

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO products
+    (name,
+     description,
+     price,
+     stock)
+VALUES (
+        'External SSD',
+        '1 TB portable external SSD',
+        1490.00,
+        10);
